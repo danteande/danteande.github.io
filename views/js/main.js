2c45d53b608b3ec5ff9009244372ad8cf2706fb3
danteande <dante.anderson@gmail.com>
2017-02-27 23:38:16 -0800

    copying stuff a bit

--- views/js/main.js
+++ views/js/main.js
@@ -449,11 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    var thepizzaslist = document.querySelectorAll(".randomPizzaContainer");
 
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < thepizzaslist.length; i++) {
+
+      thepizzaslist[i].style.width = newwidth +"%";
     }
   }
 
