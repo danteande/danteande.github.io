c105b93579d98ec88da5f8f78706f73874156113
danteande <dante.anderson@gmail.com>
2017-03-08 00:16:25 -0800

    looking at main.js now simplifying phase calc5

--- views/js/main.js
+++ views/js/main.js
@@ -517,8 +517,8 @@ function updatePositions() {
   var phasemultiplier = [0.7010008989737511,0.978856176516625,0.3567555995966555,-0.5933444303297376, -0.9979263273590055];
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    console.log("i%5: " + (i % 5) + " phasem: " + phasemultiplier[(i % 5)] + " phase: " + phase + " basicleft: " + items[i].basicLeft + " result: " + items[i].style.left);
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    console.log("i%5: " + (i % 5) + " phasem: " + phasemultiplier[(i % 5)] + " phase: " + phase + " basicleft: " + items[i].basicLeft + " result: " + items[i].style.left + " dbst: " + document.body.scrollTop);
+    items[i].style.left = items[i].basicLeft + (100 * phasemultiplier[(i % 5)]) + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
