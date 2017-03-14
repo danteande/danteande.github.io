001525d3051066fe16028c6eb11c7bd81ada8c9d
danteande <dante.anderson@gmail.com>
2017-03-13 23:10:52 -0700

    less pies testing variables

--- views/js/main.js
+++ views/js/main.js
@@ -521,7 +521,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   var h = window.innerHeight;
-  var rows = h/s
+  var rows = h/100
   pizzasNeeded = h
   for (var i = 0; i < 24; i++) {
     var elem = document.createElement('img');
@@ -530,7 +530,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
-    console.log("elem.basicLeft " + (i % cols)+ " s " + s +" rows " + rows);
+    console.log("elem.basicLeft " + (i % cols)+ " h " + h +" rows " + rows);
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
